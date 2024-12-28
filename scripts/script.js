32da3aaa4eb6a74af7f0cb966724060761284efb
Reza ZadDadash <reza.zdsh@gmail.com>
2024-12-29 00:31:18 +0330

    fix: mobile menu no longer visible in larger screens

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,17 @@
-const navBtn = document.querySelector(".nav__btn");
-const navMobile = document.querySelector(".nav__mobile");
-let navOpen = false;
+document.addEventListener("DOMContentLoaded", function() {
+    const navBtn = document.querySelector(".nav__btn");
+    const navMobile = document.querySelector(".nav__mobile");
+    let navOpen = false;
 
-navBtn.addEventListener("click", function() {
-if (navOpen) {
-navBtn.classList.remove("navbtn--open");
-navMobile.classList.remove("nav__mobile--open");
-navOpen = false;
-} else {
-navBtn.classList.add("navbtn--open");
-navMobile.classList.add("nav__mobile--open");
-navOpen = true;
-}
+    navBtn.addEventListener("click", function() {
+        if (navOpen) {
+            navBtn.classList.remove("navbtn--open");
+            navMobile.classList.remove("nav__mobile--open");
+            navOpen = false;
+        } else {
+            navBtn.classList.add("navbtn--open");
+            navMobile.classList.add("nav__mobile--open");
+            navOpen = true;
+        }
+    });
 });
