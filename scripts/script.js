a4997f35f06700ee7030385e515ba223b15b32bb
Reza ZadDadash <reza.zdsh@gmail.com>
2024-12-28 23:45:50 +0330

    resposive navigation bar created

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const navBtn = document.querySelector(".nav__btn");
+const navMobile = document.querySelector(".nav__mobile");
+let navOpen = false;
+
+navBtn.addEventListener("click", function() {
+if (navOpen) {
+navBtn.classList.remove("navbtn--open");
+navMobile.classList.remove("nav__mobile--open");
+navOpen = false;
+} else {
+navBtn.classList.add("navbtn--open");
+navMobile.classList.add("nav__mobile--open");
+navOpen = true;
+}
+});
